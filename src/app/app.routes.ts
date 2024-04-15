e295c4bda6a8b046e8ec6913a9d7a2226de02ada
pedias11 <pedias11@gmail.com>
2024-04-15 20:00:23 +0300

    reactive forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,22 @@ import { ForDirectiveExampleComponent } from './components/for-directive-example
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
+import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
 
 export const routes: Routes = [
-  {
-    path: 'component-input-example',
-    component: ComponentInputExampleComponent,
-  },
-  {
-    path: 'for-directive-example',
-    component: ForDirectiveExampleComponent,
-  },
-  { path: 'event-bind-example', component: EventBindExampleComponent },
-  { path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
-  { path: 'component-output-example', component: ComponentOutputExampleComponent},
-  { path: 'template-driven-form-example', component: TemplateDrivenFormExampleComponent},
-  { path: '', component: WelcomeComponent },
+  { path: 'component-input-example',
+    component: ComponentInputExampleComponent },
+  { path: 'for-directive-example',
+    component: ForDirectiveExampleComponent },
+  { path: 'event-bind-example', 
+    component: EventBindExampleComponent },
+  { path: 'simple-datatable-example', 
+    component: SimpleDatatableExampleComponent },
+  { path: 'component-output-example', 
+    component: ComponentOutputExampleComponent },
+  { path: 'template-driven-form-example', 
+    component: TemplateDrivenFormExampleComponent },
+  { path: 'reactive-form-example',
+    component: ReactiveFormExampleComponent },
+  { path: '', component: WelcomeComponent }
 ];
