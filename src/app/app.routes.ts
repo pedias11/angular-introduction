1ac94ce7aa0c6ea30d296cbd525e37e09c6337b7
pedias11 <pedias11@gmail.com>
2024-04-20 16:17:13 +0300

    http client service example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { SimpleDatatableExampleComponent } from './components/simple-datatable-e
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
+import { HttpClientExampleComponent } from './components/http-client-example/http-client-example.component';
 
 export const routes: Routes = [
   { path: 'component-input-example',
@@ -23,5 +24,7 @@ export const routes: Routes = [
     component: TemplateDrivenFormExampleComponent },
   { path: 'reactive-form-example',
     component: ReactiveFormExampleComponent },
+  { path: 'http-client-example',
+    component: HttpClientExampleComponent },
   { path: '', component: WelcomeComponent }
 ];
--- src/app/shared/services/jokes.service.ts
+++ src/app/shared/services/jokes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { ChuckNorrisJoke, DadJoke } from '../interfaces/jokes';
 
 const DAD_JOKES_API_URL = 'https://icanhazdadjoke.com/';
 const CHUCK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
@@ -8,6 +9,21 @@ const CHUCK_NORRIS_JOKES_API_URL = 'https://api.chucknorris.io/jokes/random';
   providedIn: 'root'
 })
 export class JokesService {
-
   http: HttpClient = inject(HttpClient);
+
+  getDadJoke() {
+    return this.http.get<DadJoke>(DAD_JOKES_API_URL, {
+      headers: {
+        Accept: 'application/json',
+      }
+    });
+  }
+
+  getChuckNorrisJoke() {
+    return this.http.get<ChuckNorrisJoke>(CHUCK_NORRIS_JOKES_API_URL, {
+      headers: {
+        Accept: 'application/json',
+      }
+    });
+  }
 }
