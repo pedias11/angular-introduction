70b7abb55bc6bb42e1c997cc65a6437dd5267bbf
pedias11 <pedias11@gmail.com>
2024-04-24 16:30:03 +0300

    user registration form

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -18,6 +18,7 @@ export class ListGroupMenuComponent {
     {text: 'Component Output Example', routerLink: 'component-output-example'},
     {text: 'Template Driven Form Example', routerLink: 'template-driven-form-example'},
     {text: 'Reactive Form Example', routerLink: 'reactive-form-example'},
-    {text: 'HTTP Client Example', routerLink: 'http-client-example'}
+    {text: 'HTTP Client Example', routerLink: 'http-client-example'},
+    {text: 'User Registration Example', routerLink: 'user-registration-example'}
   ];
 }
